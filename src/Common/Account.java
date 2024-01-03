d9a6ae93d4340077ce45a93c557ed19f447aa43f
giorg <giorgosmarkianos@gmail.com>
2024-01-03 23:57:06 +0200

    2

--- src/Common/Account.java
+++ src/Common/Account.java
@@ -9,4 +9,35 @@ public class Account implements Serializable{
 
     public List<Message> messageBox;
 
+    public int index=0;
+
+    Account(int id, String name){
+
+        authToken=id;
+
+    }
+
+    public int getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(int authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Message> getMessageBox() {
+        return messageBox;
+    }
+
+    public void setMessageBox(List<Message> messageBox) {
+        this.messageBox = messageBox;
+    }
 }
--- src/Server/MessagingServer.java
+++ src/Server/MessagingServer.java
@@ -1,7 +1,41 @@
 package Server;
+import Common.Interface;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class MessagingServer {
-    public static void main(String[] args){
+public class MessagingServer extends UnicastRemoteObject implements Interface {
+    public MessagingServer() throws RemoteException {
+        super();
+    }
+    int authToken=1000;
+    @Override
+    public int createAccount(String name) throws RemoteException {
+
+        return 0;
+    }
+
+    @Override
+    public void showAccounts() throws RemoteException {
+
+    }
+
+    @Override
+    public void sendMessage(String recipient, String text, int token) throws RemoteException {
+
+    }
+
+    @Override
+    public void showInbox(int token) throws RemoteException {
+
+    }
+
+    @Override
+    public void readMessage(int m_id) throws RemoteException {
+
+    }
+
+    @Override
+    public void deleteMessage(int m_id) throws RemoteException {
 
     }
 }
